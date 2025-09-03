e6b0913a15e0217bf506fd0e1eeb49eab2432272
Saele10 <sarah.oldani1003@gmail.com>
2025-09-03 14:07:01 +0200

    add jauge multiply + lim 5 crash + bonus malus editable dans editor

--- Assets/_Project/Scripts/vanManager.cs
+++ Assets/_Project/Scripts/vanManager.cs
@@ -5,11 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class VanManager : MonoBehaviour
 {
-    public static float points = 0;
+    public static float points = 0f;
     public static bool crashed = false;
+    [SerializeField] private int crashCount = 0;
     public BoxCollider2D boxCollider;
     [SerializeField] private TextMeshProUGUI _UIPoints;
     [SerializeField] private GameObject _camera;
+
+    [SerializeField] private float bonus1 = 5f;
+    [SerializeField] private float bonus2 = 10f;
+    [SerializeField] private float malus1 = 5f;
+    [SerializeField] private float malus2 = 10f;
+
+    [SerializeField] private float jauge = 1f;
+
+
+    
+// 4 crash donc 5eme GO 
+// jauge mutiply *1 *2* *3
 
     void Start()
     {
@@ -23,8 +36,15 @@ public class VanManager : MonoBehaviour
         if (collision.gameObject.CompareTag("Wall"))
         {
             Debug.Log("Player collided with a wall.");
-            crashed = true;
-            SceneManager.LoadScene("EndScreens");
+            jauge++;
+            crashCount++;
+            if (crashCount == 5)
+            {
+                Debug.Log("Game Over.");
+                crashed = true;
+                SceneManager.LoadScene("EndScreens");
+            }
+            
         }
 
     }
@@ -35,43 +55,41 @@ public class VanManager : MonoBehaviour
         {
             points = 0;
         }
+        if (jauge > 3)
+        {
+            jauge = 3;
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         switch (collision.gameObject.tag)
         {
             case "+5":
-                points += 5;
+                points += bonus1 * jauge;
+                jauge++;
                 Debug.Log("Points: " + points);
                 Destroy(collision.gameObject);
                 break;
             case "+10":
-                points += 10;
+                points += bonus2 * jauge;
+                jauge++;
                 Debug.Log("Points: " + points);
                 Destroy(collision.gameObject);
                 break;
             case "-5":
-                points -= 5;
+                points -= malus1;
                 Debug.Log("Points: " + points);
                 Destroy(collision.gameObject);
                 break;
             case "-10":
-                points -= 10;
+                points -= malus2;
                 Debug.Log("Points: " + points);
                 Destroy(collision.gameObject);
                 break;
-            case "x2":
-                points *= 2;
-                Debug.Log("Points: " + points);
-                Destroy(collision.gameObject);
-                break;
-            case "/2":
-                points *= 0.5f;
-                Debug.Log("Points: " + points);
-                Destroy(collision.gameObject);
-                break;
+
             case "Oil":
                 Debug.Log("Player hit oil.");
+                jauge++;
                 StartCoroutine(ScreenRotate());
                 Destroy(collision.gameObject);
                 break;
