e505f9d67110373ed6e116e50fb27f9dfe32dca8
Manon Wimmer <manonwimmer30@gmail.com>
2025-09-04 16:12:55 +0200

    [ADD] Kirby random direction left or right + deactivate with zone (except roads)

--- Assets/_Project/Scripts/Movement/MovingObject.cs
+++ Assets/_Project/Scripts/Movement/MovingObject.cs
@@ -2,9 +2,42 @@ using UnityEngine;
 
 public class MovingObject : MonoBehaviour
 {
+    // ----- FIELDS ----- //
+    // Kirby
+    private bool _isKirby = false;
+    private Vector3 _kirbyDirection;
+    // ----- FIELDS ----- //
+
+    private void Start()
+    {
+        _isKirby = gameObject.CompareTag("Kirby");
+        GetKirbyRandomDirection();
+    }
+
+    private void OnEnable()
+    {
+        GetKirbyRandomDirection();
+    }
+
+    private void GetKirbyRandomDirection()
+    {
+        if (_isKirby)
+        {
+            int random = Random.Range(0, 1);
+
+            if (random == 0)
+                _kirbyDirection = new Vector3(.75f, 0, 0);
+            else
+                _kirbyDirection = new Vector3(-.75f, 0, 0);
+        }
+    }
+
     void Update()
     {
         // Move object down
-        transform.Translate(-transform.up * Time.deltaTime * RandomGenerator.Instance.CurrentSpeed);
+        if (_isKirby)
+            transform.Translate((-transform.up + _kirbyDirection) * Time.deltaTime * RandomGenerator.Instance.CurrentSpeed);
+        else
+            transform.Translate(-transform.up * Time.deltaTime * RandomGenerator.Instance.CurrentSpeed);
     }
 }
